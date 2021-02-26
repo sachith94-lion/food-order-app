01c1b3784031e8814347a5172daaccd76644d2ee
sachith94-lion <sachithmanuradha@gmail.com>
2021-02-26 15:31:43 +0530

    project setup

---
+++ food order/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  food order
+//
+//  Created by Lionel Rajapakse on 2021-02-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ food orderTests/food_orderTests.swift
@@ -0,0 +1,33 @@
+//
+//  food_orderTests.swift
+//  food orderTests
+//
+//  Created by Lionel Rajapakse on 2021-02-26.
+//
+
+import XCTest
+@testable import food_order
+
+class food_orderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
